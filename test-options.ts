cede7264b642b996e07c9a5054ebf781b40276cd
Davit Ebralidze <Davit.Ebralidze@ventionteams.com>
2024-09-13 13:44:33 +0400

    Adding fixtures

--- test-options.ts
+++ test-options.ts
@@ -1,19 +1,19 @@
-import { test as base } from '@playwright/test'
-import { PageManager } from './page-objects/pageManager';
+import { test as base } from "@playwright/test";
+import { PageManager } from "./page-objects/pageManager";
 
 export type TestOptions = {
-    setup: string;
-    pm: PageManager;
-}
+  setup: string;
+  pm: PageManager;
+};
 
 export const test = base.extend<TestOptions>({
-    setup: async({page}, use) => {
-        await page.goto('/');
-        await use('');
-        await page.close();
-    },
-    pm: async({page , setup}, use) => {
-        const pm = new PageManager(page);
-        await use(pm);
-    }
-})
+  setup: async ({ page }, use) => {
+    await page.goto("/");
+    await use("");
+    await page.close();
+  },
+  pm: async ({ page, setup }, use) => {
+    const pm = new PageManager(page);
+    await use(pm);
+  },
+});
