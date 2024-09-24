2b7f67273cc43e4738dd73f8c0a0db6a8256bd2c
Davit Ebralidze <Davit.Ebralidze@ventionteams.com>
2024-09-24 15:02:50 +0400

    Removing unnecessary import from button-element.ts

--- page-components/button-element.ts
+++ page-components/button-element.ts
@@ -1,5 +1,4 @@
 import { Locator } from '@playwright/test';
-import { test } from '../page-fixtures/test-options.ts'
 import { BaseElement } from './base-element.ts';
 
 export class ButtonElement extends BaseElement{
