635a018d6f08f755c7b1a1f029ee34fc8d12ae17
David Ebralidze <dav.ebralidze@gmail.com>
2024-10-04 21:33:58 +0400

    Learning how to save token as temp var

---
+++ tests/api-token.spec.ts
@@ -0,0 +1,24 @@
+import {test, expect} from '@playwright/test'
+
+test('API Login and store token', async ({ request }) => {
+const response = await request.post('https://reqres.in/api/login', {
+    data: {
+        "email": "eve.holt@reqres.in",
+        "password": "cityslicka"
+    }
+});
+
+expect(response.status()).toBe(200);
+
+const responseBody = await response.json();
+
+expect(responseBody).toHaveProperty('token');
+
+const apiToken = responseBody.token;
+
+console.log(`Token received: ${apiToken}`);
+
+process.env.API_TOKEN = apiToken;
+
+console.log('Token recieved from env temp var: ' + process.env.API_TOKEN)
+});
