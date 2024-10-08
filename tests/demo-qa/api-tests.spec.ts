0ca286153739eaf44ac6337655ef349c5c74f5b4
Davit Ebralidze <Davit.Ebralidze@ventionteams.com>
2024-10-08 14:36:25 +0400

    Adding API registration tests

--- tests/demo-qa/api-tests.spec.ts
+++ tests/demo-qa/api-tests.spec.ts
@@ -1,33 +1,23 @@
 import { test, expect } from "@playwright/test"
 import { faker } from "@faker-js/faker"
 
-
-test('ENV TESTING', async ({page}) => {
-    console.log(process.env.USERNAME)
-    process.env.USERNAME = "OVERRIDEN"
-    console.log(process.env.USERNAME)
-})
-
-test('Gemerate Token', async ({request})=> {
-    const response = await request.post(`${process.env.DEMOQA_URL}/Account/v1/GenerateToken`, {
+test('Registering with valid credentials', async ({request})=> {
+    const response = await request.post(`${process.env.DEMOQA_URL}/Account/v1/User`, {
         data: {
-            userName: "testuser123123123123",
-            password: "AAAAaaaa1111!!!!"
+            userName: faker.internet.userName(),
+            password: "ASDasd!@#123"
         }
     })
-    expect(response.status()).toBe(200)
-
-    const responseBody = await response.json();
-    expect(responseBody).toHaveProperty('token');
-    process.env.API_KEY = responseBody.token;
+    expect(response.status()).toBe(201)
 })
 
-test('Login with API', async ({request})=> {
-    const response = await request.post(`${process.env.DEMOQA_URL}/Account/v1/Login`, {
+test('Registering with invalid credentials', async ({request})=> {
+    const response = await request.post(`${process.env.DEMOQA_URL}/Account/v1/User`, {
         data: {
-            userName: "testuser123123123123",
-            password: "AAAAaaaa1111!!!!"
+            userName: faker.string.alphanumeric(500),
+            password: "ASDasd!@#123"
         }
     })
-    expect(response.status()).toBe(200)
+    console.log(faker.string.alphanumeric(1000))
+    expect(response.status()).toBe(502)
 })
