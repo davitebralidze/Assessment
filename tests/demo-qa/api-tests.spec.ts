72e7047f72afba5f74cc68f5a2315f4de04b85f9
Davit Ebralidze <Davit.Ebralidze@ventionteams.com>
2024-10-09 12:36:27 +0400

    Removing unnecessary import

--- tests/demo-qa/api-tests.spec.ts
+++ tests/demo-qa/api-tests.spec.ts
@@ -5,6 +5,7 @@ import { Endpoint, HTTPMethod } from "../../enums/API-enums"
 import { Utils } from "../../utils/utils"
 
 test('Registering with valid credentials', async ({APIFixture})=> {
+    
     const credentials = 
         {
             userName: await Utils.generateFakeUsername(),
@@ -12,6 +13,10 @@ test('Registering with valid credentials', async ({APIFixture})=> {
         };
 
     const response = await API.makeCall(HTTPMethod.POST, Endpoint.Register, credentials)
+
+    console.log("Response Status:", response.status());
+    console.log("Response Body:", await response.json());
+
     expect(response.status()).toBe(201)
 
 })
